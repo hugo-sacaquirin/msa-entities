26d42a6e79059afb756ac53691bbe0272e2921ff
Hugo Sacaquirin <hugosacaquirin@hotmail.com>
2025-12-14 20:38:46 -0500

    add logs transactional

--- src/main/java/com/sipeip/MsaEntitiesApplication.java
+++ src/main/java/com/sipeip/MsaEntitiesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsaEntitiesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsaEntitiesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsaEntitiesApplication.class, args);
+    }
 
 }
